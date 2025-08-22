35f0a4a1f660cd152ed22cfb622cb5d61206053a
arshsc <arsh.chowdhary@ucas-edu.net>
2025-08-22 10:50:56 -0600

    who are you project finished

---
+++ projects/who_are_you.py
@@ -0,0 +1,7 @@
+#AC 2nd Who Are You
+
+while 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+fav_color = input("Enter your favorite color: ")
+print("Hello", name + ", you are", age, "years old, and your favorite color is", fav_color + ".")

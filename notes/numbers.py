46cfad59827e99ec940cf3d14992cee1d1962270
arshsc <arsh.chowdhary@ucas-edu.net>
2025-08-27 10:50:28 -0600

    number notes

---
+++ notes/numbers.py
@@ -0,0 +1,15 @@
+# AC 2nd Integers and Floats Notes
+
+# Integers
+age = 654654649999999999999999999999999
+temp = 85
+attention = -8
+spoons = 4
+
+#Float
+pi = 3.14
+pounds = 5.6
+emotional_damage = .99
+patience_level = -.50
+
+print(8/4+7 - (8+2)%4**2)

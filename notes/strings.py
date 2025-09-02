1c5352b0cad2ddeb125b497b7c1a17d36bd2c6ec
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-02 10:51:06 -0600

    almost finish strings notes

--- notes/strings.py
+++ notes/strings.py
@@ -1,2 +1,26 @@
 #AC 2nd Strings Note
 
+# Examples
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = 'Xavier'
+month = 'September'
+food = "chocolate"
+last_name = "LaRose"
+
+# If I need to use ' or "
+name = "La'Rose"
+best = '"The Return of the King is the best book ever!'
+
+# Length of strings
+length = len(month)
+print("September has", length, "letters.")
+
+# Escape Character
+print('\tWill said: \n"Don\'t die"')
+
+# Concatenate
+full_name = first_name + " " + last_name
+print(full_name)
+
+# Slice
+print(last_name[2:6])

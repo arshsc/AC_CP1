4d6e6c57c1cb534406f4028ac53e8d72edb31485
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-03 10:46:40 -0600

    finished madlib practice

---
+++ practices/madlib.py
@@ -0,0 +1,16 @@
+# AC 2nd Madlib
+
+adjective = input("Enter an adjective: ")
+type_of_bird = input("Enter a type of bird: ")
+room = input("Enter a room in a house: ")
+verb_past_tense = input("Enter a past tense verb: ")
+verb = input("Enter a verb: ")
+relative_name = input("Enter a relative's name: ")
+noun = input("Enter a noun: ")
+liquid = input("Enter a type of liquid: ")
+verb_ending_ing = input("Enter a verb ending in 'ing': ")
+part_of_body = input("Enter a plural part of the body: ")
+plural_noun = input("Enter a plural noun: ")
+
+madlib = "It was a " + adjective + ", cold November day. I woke up to the " + adjective + " smell of " + type_of_bird + " roasting in the " + room + " downstairs. I " + verb_past_tense + " down the stairs to see if I could help " + verb + " the dinner. My mom said, \"See if " + relative_name + " needs a fresh " + noun + ".\" So I carried a tray of glasses full of " + liquid + " into the " + verb_ending_ing + " room. When I got there, I couldn't believe my " + part_of_body + "! There were " + plural_noun + " " + verb_ending_ing + " on the " + noun + "!"
+print(madlib)

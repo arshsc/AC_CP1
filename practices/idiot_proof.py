effce7b1accb731101b7d4669d3b69d20778e5b0
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-04 10:50:57 -0600

    started idiot proof assignment

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,1 +1,7 @@
 #AC 2nd Idiot Proof
+
+first_name = input("What is your first name: ").strip().capitalize()
+last_name = input("What is your last name: ").strip().capitalize()
+phone = int(input("What is your phone number: "))
+gpa = float(input("what is your GPA: "))
+print(phone.replace("-", " ")) 

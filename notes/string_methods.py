00483d69e9deefb79981428342314b0e080134d5
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-04 10:36:14 -0600

    strings method notes

---
+++ notes/string_methods.py
@@ -0,0 +1,20 @@
+#AC 2nd String Methods
+
+# .lower() => makes it all lowercase
+# .upper() => makes it all uppercase
+# .capitalize() => capitalizes the first letter
+# .title() => capitalizes the first letter of every word
+
+age = input("Bro how old are you?")
+name = input("What is your name").strip().title()
+print("Hello {}, it is nice to meet you! You are {} years old!".format(name, age))
+
+#age = input("Bro how old are you?")
+#print(age.isdigit())
+#print("Really? " + age + "is old.")
+
+#sentence = "The quick brown fox jumps over the lazy dog!"
+#word = "brown"
+#print(sentence.find(word))
+#length = len(word)
+#print(sentence.replace(word, "yellow"))

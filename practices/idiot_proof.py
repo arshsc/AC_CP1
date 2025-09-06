9ddf26ad02ba9878a757dbed740a1f56d24b9259
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-06 03:20:09 +0000

    finished idiot proof

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -4,6 +4,4 @@ full_name = input("What is your full name: ").strip().title()
 phone = input("What is your phone number: ").strip().replace("-", " ")
 gpa = float(input("what is your GPA: ").strip())
 
-phone_is_digit = phone.isdigit()
-
-print(f"Name: {full_name}\nPhone Number: {phone_is_digit}\nGPA: {gpa:.1f}")
+print(f"Name: {full_name}\nPhone Number: {phone}\nGPA: {gpa:.1f}")

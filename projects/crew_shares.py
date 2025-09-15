2911853e820dd3ab004696e037691f367d187427
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-15 10:40:15 -0600

    finished crew shares project

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,11 +1,18 @@
 # AC 2nd Crew Shares
 
 money_earned = float(input("How much money was earned: "))
-crew_count = float(input("How many crew members are there: "))
+crew_count = int(input("How many crew members are there: "))
 
-money_earned = money_earned - (crew_count * 500)
-print(money_earned)
+money_after_divvy = money_earned - (crew_count * 500)
+crew_count_updated = crew_count - 2
+share = (money_after_divvy / crew_count_updated)
 
-share = (money_earned / crew_count) * 2
-captain_money_amount = share * 7
-print(captain_money_amount)
+captain_shares = share * 7
+first_mate_shares = share * 3
+crew_shares = share - 500
+
+print(f"\nHow much was earned: ${money_earned:,.2f}")
+print(f"How many crew members are there: {crew_count}")
+print(f"The captain gets: ${captain_shares:,.2f}")
+print(f"The first mate gets: ${first_mate_shares:,.2f}")
+print(f"The crew still needs: ${crew_shares:,.2f}")

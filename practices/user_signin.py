ca745be6db5dd5d8b249e48ead791b620a6369da
arshsc <arsh.chowdhary@ucas-edu.net>
2025-09-19 10:50:16 -0600

    finished user sign in project

---
+++ practices/user_signin.py
@@ -0,0 +1,9 @@
+# AC 2nd User Sign In
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "exampleuser321" and password == "examplepass123":
+    print(f"Welcome {username}!")
+else:
+    print(f"Username or Password is incorrect.")

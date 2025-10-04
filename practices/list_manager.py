8af8f9f0e6a6f32afbeca7e85f0edd0bbfce3c68
arshsc <arsh.chowdhary@ucas-edu.net>
2025-10-04 01:44:25 +0000

    doing list manager

--- practices/list_manager.py
+++ practices/list_manager.py
@@ -1,8 +1,26 @@
 # AC 2nd Shopping List Manager
 
+shopping_list = []
+
 while True:
-    action = #user input with instructions
-    #Write your code here
-    
+    action = input("1. Add item to shopping list:\n2. Remove item from shopping list\n3. Print shopping list\n")
+    if action == "1":
+        addition_option = input("What single item would you like to add to the list: ")
+        shopping_list.append(addition_option)
+    elif action == "2":
+        remove_option = input("1. Empty list\n 2. Remove singular item?")
+        if remove_option == "1":
+            shopping_list.clear()
+        elif remove_option == "2":
+            #pass
+            print(shopping_list)
+            item_removed = input("What item would you like to be removed from shopping list: ")
+            shopping_list.remove(item_removed)
+        else:
+            print("Enter valid option")
+    elif action == "3":
+        print(shopping_list)
     elif action == "exit":
         break
+    else:
+        print("Enter valid option")

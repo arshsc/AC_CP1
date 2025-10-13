a38ea4b69292a93287223098036cf3d78147a336
arshsc <arsh.chowdhary@ucas-edu.net>
2025-10-13 10:50:40 -0600

    finished password strength

--- projects/password_strength.py
+++ projects/password_strength.py
@@ -11,19 +11,32 @@ lowercase_req = False
 number_req = False
 special_char_req = False
 
-# Step 2: Check the factors
-while True:
+# Define Special Characters
+special_characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ".", ",", ":", ";", "<", ">", "?", "[", "]", "{", "}", "|"]
+
+# Step 2: Check the factors, if any requirements meet set the boolean to true
     # Atleast 8 Character Length
-    if len(password) >= 8:
-         length_req = True
-         score += 1
-         continue
+if len(password) >= 8:
+    length_req = True
+    score += 1
     # Atleast 1 Uppercase letter
-    check_upper = any(char.isupper() for char in password)
-    print(check_upper)
+if any(char.isupper() for char in password):
+    uppercase_req = True
+    score += 1
     # Atleast 1 Lowercase letter
+if any(char.islower() for char in password):
+    lowercase_req = True
+    score += 1
     # Atleast 1 Number
+if any(char.isdigit() for char in password):
+    number_req = True
+    score += 1
     # Atleast 1 Special Character
+for item in special_characters:
+    if item in password:
+        special_char_req = True
+        score += 1
+        break
 
 # Step 3: Calculate the strength
 # Length Requirement: +1 point
@@ -32,6 +45,18 @@ while True:
 # Containing a number: +1 point
 # Containing a special character: +1 point
 
+# Display Password Strength Assessment
+print(f"\nPassword Strength Assessment:\nLength (8+ Characters): {length_req}\nContains Uppercase: {uppercase_req}\nContains Lowercase: {lowercase_req}\nContains a Number: {number_req}\nContains a Special Character: {special_char_req}\n")
+
 # Step 4: Give a strength score
 # Add the points up and print with corresponding level of strength
-# Tell the user what their missing in the password
+if score == 1 or score == 2:
+    print(f"Strength Score: {score}/5\nPassword Strength: Weak")
+elif score == 3:
+    print(f"Strength Score: {score}/5\nPassword Strength: Moderate")
+elif score == 4:
+    print(f"Strength Score: {score}/5\nPassword Strength: Strong")
+elif score == 5:
+    print(f"Strength Score: {score}/5\nPassword Strength: Very Strong")
+else:
+    print("This shouldn't be possible")

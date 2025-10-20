cb47de1f0fe262df9ea4b93d42957b2551e7ee89
arshsc <arsh.chowdhary@ucas-edu.net>
2025-10-20 10:51:06 -0600

    started functiones notes

---
+++ notes/functions.py
@@ -0,0 +1,13 @@
+# AC 2nd Functions Notes
+
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num += 1
+print("Goose")
+print(f"Results is: {add(-57236434,9264387)}")
+total = add(3874687,328473)
+print(add(add(3.14,.85),10)
+add(42,7)

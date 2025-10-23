2235f6f4a7c662f53f23b0492761365098021a8e
arshsc <arsh.chowdhary@ucas-edu.net>
2025-10-23 10:22:07 -0600

    small updates to caesar cipher

--- practices/caesar_cipher.py
+++ practices/caesar_cipher.py
@@ -7,6 +7,6 @@ print(f"100 = {chr(100)}")
 def encode(shift, message):
 
     
-choice = input("Choose operaton:\n(1) Encode\n(2) Decode")
-message = input("Enter the message: ")
-shift_value = input("Enter a shift value as an integer: ")
+choice = input("Choose operaton:\n(1) Encode\n(2) Decode\n")
+message = input("Enter the message:\n")
+shift_value = input("Enter a shift value as an integer:\n")

088ac40db065ae555f7d9faf0e149f832063988d
arshsc <arsh.chowdhary@ucas-edu.net>
2025-11-03 10:51:12 -0700

    doing maze_generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -3,6 +3,50 @@
 import turtle
 import random
 
-maze_rows = [[], [], [], [], [], []]
-maze_column = [[], [], [], [], [], []]
-# 6x6 Random Maze
+def is_solvable(row_grid, col_grid):
+    size = len(row_grid) - 1
+    visited = set()
+    stack = [(0,0)]
+
+    while stack:
+        x, y = stack.pop()
+        if x == size - 1 and y == size - 1:
+            return True
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        if x < size - 1 and col_grid[y][x + 1] == 0:
+            stack.append((x + 1, y))
+        if y < size - 1 and row_grid[y + 1][x] == 0:
+            stack.append((x, y + 1))
+        if x > 0 and col_grid[y][x] == 0:
+            stack.append((x - 1, y))
+        if y > 0 and row_grid[y][x] == 0:
+            stack.append((x, y - 1))
+    return False
+
+maze_rows = [[random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ]]
+
+maze_cols = [[random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ],
+             [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), ]]
+
+turtle.Turtle()
+turtle.penup()
+
+for grid in maze_rows:
+    if grid == 0:
+        turtle.penup()
+    if grid == 1:
+        turtle.pendown()
+        turtle.forward(10)
+
+turtle.done()

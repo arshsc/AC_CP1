0dc9cb27821ce755872c9237fa69aa226ae6fe18
arshsc <arsh.chowdhary@ucas-edu.net>
2025-11-10 10:51:10 -0700

    order up started and ifnished dictionary notes

---
+++ practices/order.py
@@ -0,0 +1,37 @@
+# AC 2nd Order Up
+
+# Define the menu dictionary
+menu = {
+    # Define another dictionary for drinks
+    "drink": {
+        # List the 4 items with the key and the value as the price
+        "Water": 0.00,
+        "Fountain Drink": 2.49,
+        "Lemonade": 2.79,
+        "Sweet Tea": 2.49
+    },
+    # Define another dictionary for the main course
+    "main course": {
+        "The Box Combo": 11.99,
+        "The 3 Finger Combo": 10.39,
+        "The Caniac Combo": 17.29,
+        "": 
+    },
+    # Define another dictionary for the side dishes
+    "side dishes": {
+        "": ,
+        "": ,
+        "": ,
+        "": 
+    }
+}
+
+# Display the menu
+
+# Ask the user what they want to drink
+
+# Ask the user what they want as the main course
+
+# Ask the user what they want as their 2 side dishes
+
+# Repeat their order with the total cost.

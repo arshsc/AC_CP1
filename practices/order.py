55fa418c13cb835ceea59b4b5701118c5ff30e4f
arshsc <arsh.chowdhary@ucas-edu.net>
2025-11-15 05:25:43 +0000

    finsihed order up

--- practices/order.py
+++ practices/order.py
@@ -2,9 +2,9 @@
 
 
 # Setting up the menu
-# Define the menu dictionary
+# Dictionary for the menu
 menu = {
-    # Define another dictionary for drinks
+    # Define a nested dictionary for the drinks
     "Drinks": {
         # List the items with the key and the value as the price
         "Water": 0.00,
@@ -13,7 +13,7 @@ menu = {
         "Sweet Tea": 2.49,
         "Unsweet Tea": 2.49
     },
-    # Define another dictionary for the main course
+    # Define a nested dictionary for the main course
     "Main Courses": {
         "The Box Combo": 11.99,
         "The 3 Finger Combo": 10.39,
@@ -21,7 +21,7 @@ menu = {
         "The Sandwich Combo": 10.79,
         "The Kids Combo": 6.99 
     },
-    # Define another dictionary for the side dishes
+    # Define a nested dictionary for the side dishes
     "Side Dishes": {
         "Chicken Finger": 1.99,
         "Fries": 2.59,
@@ -39,14 +39,15 @@ def display_menu(menu_item):
         print(f"${menu[menu_item][i]} {i}")
 
 # Function to order and add up the cost
-def ordering(order, menu_item, cost):
-    for i in menu:
-        if order in menu[menu_item]:
-            cost += menu[menu_item][order]
-            return cost
+def ordering(menu_item, prompt=None):
+    while True:
+        if prompt:
+            order = input(f"\nWhat {prompt} would you like to order? (Type in the name)\n").title().strip()
         else:
-            print("Invalid Option, Retry")
-            continue
+            order = input(f"\nWhat {menu_item[:-1].lower()} would you like to order? (Type in the name)\n").title().strip()
+        if order in menu[menu_item]:
+            return menu[menu_item][order], order
+        print("Invalid Option, Retry")
 
 
 # Calling the functions
@@ -57,24 +58,22 @@ display_menu("Main Courses")
 display_menu("Side Dishes")
 
 # Ordering
-# Set cost to $0.00
 total_cost = 0.00
 
-# Ordering the drinks
-drink_order = input("\nWhat drink would you like to order? (Type in the name)\n").title()
-total_cost += ordering(drink_order, "Drinks", total_cost)
+# Drinks
+cost, drink_order = ordering("Drinks")
+total_cost += cost
 
-# Ordering the main course
-main_course_order = input("\nWhat main course would you like to order? (Type in the name)\n").title()
-total_cost += ordering(main_course_order, "Main Courses", total_cost)
+# Main Course
+cost, main_course_order = ordering("Main Courses")
+total_cost += cost
 
-# Ordering the first side dish
-side_dish_order_1 = input("\nWhat first side would you like to order? (Type in the name)\n").title()
-total_cost += ordering(side_dish_order_1, "Side Dishes", total_cost)
+# Side Dishes
+cost, side_dish_order_1 = ordering("Side Dishes", prompt="first side")
+total_cost += cost
 
-# Ordering the second side dish
-side_dish_order_2 = input("\nWhat second side would you like to order? (Type in the name)\n").title()
-total_cost += ordering(side_dish_order_2, "Side Dishes", total_cost)
+cost, side_dish_order_2 = ordering("Side Dishes", prompt="second side")
+total_cost += cost
 
-# Printing out the reciept
+# Printing out the receipt
 print(f"\nYour Order:\nDrink: {drink_order}\nMain Course: {main_course_order}\nSides:\n{side_dish_order_1}\n{side_dish_order_2}\nTotal Cost: ${total_cost:.2f}")

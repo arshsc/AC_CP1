fc63f12038d2e806fce0cf8607f21ca3e879da63
arshsc <arsh.chowdhary@ucas-edu.net>
2025-12-11 10:51:00 -0700

    gmae work more

--- final/game.py
+++ final/game.py
@@ -35,6 +35,8 @@ miner_damage = random.randint(25, 50)
 # list of all locations
 locations = ["Trailer Home", "Collapsed House", "Mansion", "Prison", "School", "Library", "Train Station", "Entrance of the Mine", "Underground Railroad"]
 
+location = "Town"
+
 # trailer home
 trailer_home_searched = False
 bathroom_searched = False
@@ -126,6 +128,7 @@ def trailer_home(bathroom_searched, storage_cabinet_searched, main_area_searched
                     storage_cabinet_searched = True
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
                     print("\nYou open the big storage cabinet. Old clothes and jackets scatter the insides. You dig through the old clothes and find a key with an engraving that says \"School Front Door\". You stash it in your backpack.\n\n(+) School Front Door Key")
+                    inventory["Keys"].append("School Key")
                     break
                 elif storage_cabinet_searched == True:
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -197,7 +200,7 @@ def collapsed_house(living_room_searched, bedroom_searched, rubble_pile_searched
                     print("\nYou already searched the living room, nothing was here to be found.")
                     break
 
-            elif search == "storage cabinet":
+            elif search == "bedroom":
                 if bedroom_searched == False:
                     bedroom_searched = True
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -209,13 +212,13 @@ def collapsed_house(living_room_searched, bedroom_searched, rubble_pile_searched
                     print("\nYou have already searched the bedroom and found the bandage.")
                     break
 
-            elif search == "main area":
+            elif search == "rubble pile":
                 if rubble_pile_searched == False:
                     rubble_pile_searched = True
                     rubble_pile_searched = True
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
                     print("\nYou see a suspicious looking rubble pile. You dig through and find an unlocked chest, and in this chest is a key labeled \"Library\"\n\n(+) Library Key")
-                    inventory["Keys"]["Library Key"]
+                    inventory["Keys"].append("Library Key")
                     break
                 elif rubble_pile_searched == True:
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -233,6 +236,7 @@ def collapsed_house(living_room_searched, bedroom_searched, rubble_pile_searched
                 print("\nThat is not a valid option, please retry.")
                 break
 
+# mansion
 def mansion(master_bedroom_searched, vault_searched, garden_searched):
 
     location = "Mansion"
@@ -257,7 +261,7 @@ def mansion(master_bedroom_searched, vault_searched, garden_searched):
                     master_bedroom_searched = True
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
                     print("\nYou enter the massive bedroom. There is elegant furniture throughout the whole room. You open the nightstand and find a key labeled \"Train Station\".\n\n(+) Train Station Key")
-                    inventory["Keys"]["Train Station Key"]
+                    inventory["Keys"].append("Train Station Key")
                     break
                 elif master_bedroom_searched == True:
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -269,7 +273,7 @@ def mansion(master_bedroom_searched, vault_searched, garden_searched):
                     vault_searched = True
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
                     print("\nYou walk down the grand stairs into the basement. The only thing in this basement is a giant vault door, slightly cracked open. You peek in and see an intact glass showcase with an axe. This is not an ordinary axe, it has a mysterious dark aura around it. You pick it up and feel an eerie feeling.\n\n(+) Ghost Axe")
-                    inventory["Weapons"]["Ghost Axe"]
+                    inventory["Weapons"].append("Ghost Axe")
                     break
                 elif vault_searched == True:
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -298,8 +302,149 @@ def mansion(master_bedroom_searched, vault_searched, garden_searched):
                 print("\nThat is not a valid option, please retry.")
                 break
         
+# prison
+def prison(kitchen_searched, cell_hallway_searched, cell_searched):
 
-location = "Town"
+    location = "Prison"
+    
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("You walk over towards the massive prison gates. The door is locked but it seems easy to climb, so you do so and get in.")
+    print("\nThis prison is huge. A lot of it is boarded up and not too many rooms are available.")
+
+    while location == "Prison":
+
+        while True:
+            search = input("\nThere are 3 areas worth searching.\n   \n     - Kitchen   \n     - Cell Hallway   \n     - Cell     \n     - Exit\n\nWhere would you like to go: ").lower().strip()
+
+            if search == "kitchen" or "cell hallway" or "cell" or "exit":
+                break
+            else:
+                print("\nThat is not a room, please retry.")
+
+        while True:
+            if search == "kitchen":
+                if kitchen_searched == False:
+                    kitchen_searched = True
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou enter the kitchen. It is filthy, but some food could be nice. You open the storage room and see a bunch of food. You pick some up and realize they all expired decades ago.\n\n")
+                    break
+                elif kitchen_searched == True:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou already searched the kitchen. Nothing was to be found")
+                    break
+
+            elif search == "cell hallway":
+                if cell_hallway_searched == False:
+                    cell_hallway_searched = True
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou walk to the massive hallway. Cells on each side with a second story of more cells. On the side near the emergency exit, there seems to be some sort of battery. You pick it up.\n\n(+) Battery")
+                    inventory["Items"].append("Battery")
+                    break
+                elif cell_hallway_searched == True:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou have already searched the cell hallway and found a battery.")
+                    break
+
+            elif search == "cell":
+                if cell_searched == False:
+                    cell_searched = True
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou walk into one of the only cells not blocked. You search the small table and under the bed. You find a single wire under the bed. \n\n(+) Wire")
+                    inventory["Items"].append("Wire")
+                    break
+                elif cell_searched == True:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou have already searched the cell and found a wire.")
+                    break
+
+            elif search == "exit":
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                print("\nYou exit the Prison")
+                location = "Town"
+                break
+
+            else:
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                print("\nThat is not a valid option, please retry.")
+                break
+
+# school
+def school(classroom_searched, school_library_searched, office_searched, cafeteria_searched):
+
+    location = "School"
+    
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("You walk over towards the school. You use your school key and it works, the front door opens.")
+    print("\nThis is a relatively small and very old school.")
+
+    while location == "School":
+
+        while True:
+            search = input("\nThere are 4 areas worth searching.\n   \n     - Classroom   \n     - School Library   \n     - Office     \n     - Cafeteria     \n     - Exit\n\nWhere would you like to go: ").lower().strip()
+
+            if search == "classroom" or "school library" or "office" or "cafeteria" or "exit":
+                break
+            else:
+                print("\nThat is not a room, please retry.")
+
+        while True:
+
+            if search == "classroom":
+                if classroom_searched == False:
+                    classroom_searched = True
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou enter the classroom. The roof seems that it is about to cave in. On one of the desks, there is an unopened energy drink that seems new. You pick it up and put it in your backpack.\n\n(+) Bandage")
+                    inventory["Consumables"]["Energy Drink"] = 4
+                    break
+                elif classroom_searched == True:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou already searched the classroom, you picked up the energy drink and nothing else is to be found.")
+                    break
+
+            elif search == "school library":
+                if school_library_searched == False:
+                    school_library_searched = True
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\You walk into the tiny library. The bookshelves are on the floor, and many books scattered through the floor. Nothing useful seems to be in here.")
+                    break
+                elif school_library_searched == True:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\nYou have already searched the library and nothing was to be found.")
+                    break
+
+            elif search == "office":
+                if office_searched == False:
+                    office_searched = True
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    office_note = input("\nYou can't enter the office as it is locked shut. you peek over the front desk and find a note.\n\n     - Yes     \n     - No \n\nWould you like to read it: ").strip().lower()
+                    if office_note == "yes" or "y":
+                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        print("\n\"WHAT NOTE SAYS!\"")
+                        break
+                    elif office_note == "no" or "n":
+                        print("You put the note back down.")
+                        break
+                elif office_searched == True:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    office_note = input("\nThere is nothing else here besides the note. Would you like to read the note again: ")
+                    while True:
+                        if office_note == "yes" or "y":
+                            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                            print("\n\"WHAT NOTE SAYS!\"")
+                            break
+                        elif office_note == "no" or "n":
+                            break
+
+            elif search == "exit":
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                print("\nYou exit the school")
+                location = "Town"
+                break
+
+            else:
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                print("\nThat is not a valid option, please retry.")
+                break
 
 def room_input(locations, inventory):
 
@@ -336,7 +481,7 @@ while True:
 
     if location == "Trailer Home":
         if trailer_home_searched == False:
-            trailer_home(bathroom_searched, storage_cabinet_searched, main_area_searched)
+            trailer_home(bathroom_searched, storage_cabinet_searched,  main_area_searched)
             trailer_home_searched = True
         elif trailer_home_searched == True:
             print("\nYou have already searched the Trailer Home.")
@@ -357,3 +502,17 @@ while True:
         elif mansion_searched == True:
             print("\nYou have already searched the Mansion.")
             mansion(master_bedroom_searched = True, vault_searched = True, garden_searched = True)
+
+    elif location == "Prison":
+        if prison_searched == False:
+            prison(kitchen_searched, cell_hallway_searched, cell_hallway_searched)
+            prison_searched = True
+        elif prison_searched == False:
+            print("\nYou have already searched the Prison")
+            prison(kitchen_searched = True, cell_hallway_searched = True, cell_searched = True)
+
+    elif location == "School":
+        school_key = "School"
+        if school_searched == False:
+            if school_key in inventory["Keys"]:
+                school()
